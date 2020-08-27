de8b92e1f39491b89d2526c0038dceccbbbce554
hanseok <gkstjrajt@gmail.com>
2020-08-27 17:16:46 +0900

    회원 로그인 코드 작성

---
+++ src/web_study_09/controller/JoinController.java
@@ -0,0 +1,28 @@
+package web_study_09.controller;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/join.do")
+public class JoinController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		process(request, response);
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		process(request, response);
+	}
+
+	private void process(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		if(request.getMethod().equalsIgnoreCase("get")) {
+			request.getRequestDispatcher("member/join.jsp").forward(request, response);
+		}
+	}
+
+}
